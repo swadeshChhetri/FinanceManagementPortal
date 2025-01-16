629372f8132954a33cd99e3c95a9d4f2baf7ecf5
Swadesh Chhetri <swadeshchhetri87@gmail.com>
2025-01-16 13:25:09 +0530

    modify: route 5

--- scripts/Chart.js
+++ scripts/Chart.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const fetchData = async () => {
     try {
-      const response = await fetch("../fetch_database.json");
+      const response = await fetch("https://swadeshchhetri.github.io/FinanceManagementPortal/fetch_database.json");
       if (!response.ok) throw new Error("Failed to fetch data");
       return await response.json();
     } catch (error) {
